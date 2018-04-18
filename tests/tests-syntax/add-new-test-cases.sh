0b55066a2f0d46c3119bab93d941dca6e59aa8ac
Grigory Rechistov <grigory.rechistov@intel.com>
2018-04-18 12:41:48 +0300

    Add unit tests

--- tests/tests-syntax/add-new-test-cases.sh
+++ tests/tests-syntax/add-new-test-cases.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Read lines from stdin and create test cases for them
-let start=131
+let start=156
 while IFS='' read -r line || [[ -n "$line" ]]; do
     DIRNAME=`/usr/intel/pkgs/bash/4.4/bin/bash -c "printf test-%05d $start"`
     /bin/mkdir -p $DIRNAME
